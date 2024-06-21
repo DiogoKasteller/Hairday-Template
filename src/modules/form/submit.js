74f4f41e7a73ff0ed0f6d4af8a0636531509b086
Diogo Kasteller <diogokasteller@gmail.com>
2024-06-21 10:58:01 -0300

    solving delete bug from json-server

--- src/modules/form/submit.js
+++ src/modules/form/submit.js
@@ -40,7 +40,7 @@ form.onsubmit = async (event) => {
     const when = dayjs(selectedDate.value).add(hour, "hour")
 
     // Generates random ID
-    const id = new Date().getTime()
+    const id = new Date().getTime().toString()
 
     // Schedule
     await scheduleNew({
